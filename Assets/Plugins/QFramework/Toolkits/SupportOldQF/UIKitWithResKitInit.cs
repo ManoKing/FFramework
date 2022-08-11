7a675cb855f86937fec703290b80a40372978072
冯云龙 <fengyunlong@stary.com>
2022-08-11 15:59:24 +0800

    Add Plugins

---
+++ Assets/Plugins/QFramework/Toolkits/SupportOldQF/UIKitWithResKitInit.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace QFramework
+{
+    public class UIKitWithResKitInit
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Init()
+        {
+            UIKit.Config.PanelLoaderPool = new ResKitPanelLoaderPool();
+        }
+    }
+    public class ResKitPanelLoaderPool : AbstractPanelLoaderPool
+    {
+        public class ResKitPanelLoader : IPanelLoader
+        {
+
+            public GameObject LoadPanelPrefab(PanelSearchKeys panelSearchKeys)
+            {
+                string loadName = string.Empty;
+                if (panelSearchKeys.AssetBundleName.IsNotNullAndEmpty())
+                {
+                    loadName = panelSearchKeys.GameObjName;
+                }
+
+                if (panelSearchKeys.PanelType.IsNotNull() && panelSearchKeys.GameObjName.IsNullOrEmpty())
+                {
+                    loadName =panelSearchKeys.PanelType.Name;
+                }
+                Debug.LogWarning(loadName);
+                var op = Addressables.LoadAssetAsync<GameObject>(loadName);
+                var obj = op.WaitForCompletion();
+                return obj;
+            }
+
+            public void Unload()
+            {
+                
+            }
+        }
+
+        protected override IPanelLoader CreatePanelLoader()
+        {
+            return new ResKitPanelLoader();
+        }
+    }
+}
