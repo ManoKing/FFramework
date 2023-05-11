bbe17ecccd705f3fb4f559f8e4459f146db8fb01
DESKTOP-6Q27J9E\admin <fengyunlong@tuyoogame.com>
2023-05-11 19:23:01 +0800

    添加权限申请（TODO）

--- Assets/HybridCLRData/Generated/AOTGenericReferences.cs
+++ Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -18,10 +18,7 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 	// QFramework.Singleton<object>
 	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
 	// System.Collections.Generic.Dictionary<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
-	// System.Runtime.CompilerServices.TaskAwaiter<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
-	// System.Threading.Tasks.Task<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
 	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>
-	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>
 	// }}
 
 	public void RefMethods()
@@ -29,8 +26,6 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 		// bool QFramework.CSharpObjectExtension.IsNotNull<object>(object)
 		// object QFramework.UIKit.GetPanel<object>()
 		// object QFramework.UIKit.OpenPanel<object>(QFramework.UILevel,QFramework.IUIData,string,string)
-		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder.AwaitUnsafeOnCompleted<System.Runtime.CompilerServices.TaskAwaiter<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>,HotFix.App.<LoadScene>d__1>(System.Runtime.CompilerServices.TaskAwaiter<UnityEngine.ResourceManagement.ResourceProviders.SceneInstance>&,HotFix.App.<LoadScene>d__1&)
-		// System.Void System.Runtime.CompilerServices.AsyncVoidMethodBuilder.Start<HotFix.App.<LoadScene>d__1>(HotFix.App.<LoadScene>d__1&)
 		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<object>(object)
 		// object UnityEngine.Component.GetComponent<object>()
 	}
---
+++ Assets/Main/PermissionTool.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+public class PermissionTool : MonoBehaviour
+{
+    private string deviceName;
+    private AudioClip micRecord;
+    void Start()
+    {
+        RequestUserPermission();
+    }
+
+    public void RequestUserPermission()
+    {
+#if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+        Application.RequestUserAuthorization(UserAuthorization.Microphone);
+        if (Microphone.devices.Length > 0)
+        {
+            deviceName = Microphone.devices[0];
+            micRecord = Microphone.Start(deviceName, true, 999, 44100);
+            GetVolume();
+        }
+#endif
+    }
+
+    private float GetVolume()
+    {
+        float levelMax = 0;
+        if (Microphone.IsRecording(deviceName))
+        {
+            float[] samples = new float[128];
+            int startPosition = Microphone.GetPosition(deviceName) - (128 + 1);
+            if (startPosition >= 0)
+            {//当麦克风还未正式启动时，该值会为负值，AudioClip.GetData函数会报错
+                micRecord.GetData(samples, startPosition);
+                for (int i = 0; i < 128; i++)
+                {
+                    float wavePeak = samples[i];
+                    if (levelMax < wavePeak)
+                    {
+                        levelMax = wavePeak;
+                    }
+                }
+                levelMax = levelMax * 99;
+                // Debug.Log("麦克风音量：" + levelMax);
+            }
+        }
+        return levelMax;
+    }
+}
