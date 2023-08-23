698e71e18373debc71440a084b194150d22f2801
Phong <manoking@126.com>
2023-08-23 17:50:02 +0800

    修改link

--- Assets/HybridCLRData/Generated/AOTGenericReferences.cs
+++ Assets/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -1,24 +1,166 @@
+using System.Collections.Generic;
 public class AOTGenericReferences : UnityEngine.MonoBehaviour
 {
 
 	// {{ AOT assemblies
-	// ExtensionKit.dll
-	// SingletonKit.dll
-	// UIKit.dll
-	// Unity.Addressables.dll
-	// Unity.ResourceManager.dll
-	// UnityEngine.CoreModule.dll
-	// mscorlib.dll
+	public static readonly IReadOnlyList<string> PatchedAOTAssemblyList = new List<string>
+	{
+		"ExtensionKit.dll",
+		"SingletonKit.dll",
+		"System.Core.dll",
+		"System.dll",
+		"UIKit.dll",
+		"Unity.Addressables.dll",
+		"Unity.ResourceManager.dll",
+		"UnityEngine.CoreModule.dll",
+		"mscorlib.dll",
+	};
 	// }}
 
 	// {{ constraint implement type
 	// }} 
 
 	// {{ AOT generic types
+	// DelegateList<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0>>
+	// DelegateList<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!0>>
+	// DelegateList<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>>
+	// DelegateList<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// DelegateList<float>
+	// QFramework.DefaultObjectFactory<object>
+	// QFramework.IObjectFactory<object>
+	// QFramework.MonoSingletonProperty<object>
+	// QFramework.Pool<object>
+	// QFramework.SafeObjectPool<object>
 	// QFramework.Singleton<object>
+	// QFramework.SingletonProperty<object>
+	// System.Action<!!0>
+	// System.Action<!0>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,object>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0>>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!0>>
+	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>>
 	// System.Action<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Action<float>
+	// System.Action<object>
+	// System.Collections.Generic.ArraySortHelper<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.ArraySortHelper<object>
+	// System.Collections.Generic.Comparer<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Comparer<object>
+	// System.Collections.Generic.Dictionary.Enumerator<int,object>
+	// System.Collections.Generic.Dictionary.Enumerator<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Dictionary.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<int,object>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Dictionary.KeyCollection.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.KeyCollection<int,object>
+	// System.Collections.Generic.Dictionary.KeyCollection<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Dictionary.KeyCollection<object,object>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<int,object>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Dictionary.ValueCollection.Enumerator<object,object>
+	// System.Collections.Generic.Dictionary.ValueCollection<int,object>
+	// System.Collections.Generic.Dictionary.ValueCollection<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Dictionary.ValueCollection<object,object>
+	// System.Collections.Generic.Dictionary<int,object>
 	// System.Collections.Generic.Dictionary<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.Dictionary<object,object>
+	// System.Collections.Generic.EqualityComparer<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.EqualityComparer<int>
+	// System.Collections.Generic.EqualityComparer<object>
+	// System.Collections.Generic.HashSet.Enumerator<object>
+	// System.Collections.Generic.HashSet<object>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<int,object>>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>>
+	// System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.ICollection<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.ICollection<object>
+	// System.Collections.Generic.IComparer<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.IComparer<object>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<int,object>>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>>
+	// System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.IEnumerable<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.IEnumerable<object>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<int,object>>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>>
+	// System.Collections.Generic.IEnumerator<System.Collections.Generic.KeyValuePair<object,object>>
+	// System.Collections.Generic.IEnumerator<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.IEnumerator<object>
+	// System.Collections.Generic.IEqualityComparer<int>
+	// System.Collections.Generic.IEqualityComparer<object>
+	// System.Collections.Generic.IList<object>
+	// System.Collections.Generic.KeyValuePair<int,object>
+	// System.Collections.Generic.KeyValuePair<object,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.KeyValuePair<object,object>
+	// System.Collections.Generic.LinkedList.Enumerator<object>
+	// System.Collections.Generic.LinkedList<object>
+	// System.Collections.Generic.LinkedListNode<object>
+	// System.Collections.Generic.List.Enumerator<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.List.Enumerator<object>
+	// System.Collections.Generic.List<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.List<object>
+	// System.Collections.Generic.ObjectComparer<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.ObjectComparer<object>
+	// System.Collections.Generic.ObjectEqualityComparer<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Collections.Generic.ObjectEqualityComparer<int>
+	// System.Collections.Generic.ObjectEqualityComparer<object>
+	// System.Collections.Generic.Stack.Enumerator<object>
+	// System.Collections.Generic.Stack<object>
+	// System.Comparison<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Comparison<object>
+	// System.Func<!!0>
+	// System.Func<!0>
+	// System.Func<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0>>
+	// System.Func<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>>
+	// System.Func<object,!!0>
+	// System.Func<object,!0>
+	// System.Func<object,object>
+	// System.Func<object>
+	// System.Predicate<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle>
+	// System.Predicate<object>
+	// System.Runtime.CompilerServices.ConditionalWeakTable.CreateValueCallback<object,object>
+	// System.Runtime.CompilerServices.ConditionalWeakTable<object,object>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable.ConfiguredTaskAwaiter<!!0>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable.ConfiguredTaskAwaiter<!0>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable.ConfiguredTaskAwaiter<object>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable<!!0>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable<!0>
+	// System.Runtime.CompilerServices.ConfiguredTaskAwaitable<object>
+	// System.Runtime.CompilerServices.TaskAwaiter<!!0>
+	// System.Runtime.CompilerServices.TaskAwaiter<!0>
+	// System.Runtime.CompilerServices.TaskAwaiter<object>
+	// System.Threading.Tasks.Task.<>c<!!0>
+	// System.Threading.Tasks.Task.<>c<!0>
+	// System.Threading.Tasks.Task.<>c<object>
+	// System.Threading.Tasks.Task<!!0>
+	// System.Threading.Tasks.Task<!0>
+	// System.Threading.Tasks.Task<object>
+	// System.Threading.Tasks.TaskCompletionSource<!!0>
+	// System.Threading.Tasks.TaskCompletionSource<!0>
+	// System.Threading.Tasks.TaskCompletionSource<object>
+	// System.Threading.Tasks.TaskFactory<!!0>
+	// System.Threading.Tasks.TaskFactory<!0>
+	// System.Threading.Tasks.TaskFactory<object>
+	// UnityEngine.AddressableAssets.AddressablesImpl.<>c__DisplayClass79_0<!!0>
+	// UnityEngine.AddressableAssets.AddressablesImpl.<>c__DisplayClass79_0<object>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass60_0<!!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass60_0<!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass60_0<object>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass61_0<!!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass61_0<!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase.<>c__DisplayClass61_0<object>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase<!!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase<!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase<object>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0>
+	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!0>
 	// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>
+	// UnityEngine.ResourceManagement.ChainOperationTypelessDepedency<!!0>
+	// UnityEngine.ResourceManagement.ChainOperationTypelessDepedency<object>
+	// UnityEngine.ResourceManagement.ResourceManager.CompletedOperation<!!0>
+	// UnityEngine.ResourceManagement.ResourceManager.CompletedOperation<object>
+	// UnityEngine.ResourceManagement.Util.GlobalLinkedListNodeCache<object>
+	// UnityEngine.ResourceManagement.Util.LinkedListNodeCache<object>
 	// }}
 
 	public void RefMethods()
@@ -26,7 +168,27 @@ public class AOTGenericReferences : UnityEngine.MonoBehaviour
 		// bool QFramework.CSharpObjectExtension.IsNotNull<object>(object)
 		// object QFramework.UIKit.GetPanel<object>()
 		// object QFramework.UIKit.OpenPanel<object>(QFramework.UILevel,QFramework.IUIData,string,string)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<!!0>(object)
 		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.AddressableAssets.Addressables.LoadAssetAsync<object>(object)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.AddressableAssets.AddressablesImpl.LoadAssetAsync<!!0>(object)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.AddressableAssets.AddressablesImpl.LoadAssetAsync<object>(object)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.AddressableAssets.AddressablesImpl.LoadAssetWithChain<!!0>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,object)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.AddressableAssets.AddressablesImpl.LoadAssetWithChain<object>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,object)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.AddressableAssets.AddressablesImpl.TrackHandle<!!0>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0>)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.AddressableAssets.AddressablesImpl.TrackHandle<object>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>)
 		// object UnityEngine.Component.GetComponent<object>()
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle.Convert<!!0>()
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle.Convert<object>()
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.ResourceManagement.ResourceManager.CreateChainOperation<!!0>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,System.Func<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0>>)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.ResourceManagement.ResourceManager.CreateChainOperation<object>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,System.Func<UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object>>)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.ResourceManagement.ResourceManager.CreateCompletedOperationInternal<!!0>(!!0,bool,System.Exception,bool)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.ResourceManagement.ResourceManager.CreateCompletedOperationInternal<object>(object,bool,System.Exception,bool)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.ResourceManagement.ResourceManager.CreateCompletedOperationWithException<!!0>(!!0,System.Exception)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.ResourceManagement.ResourceManager.CreateCompletedOperationWithException<object>(object,System.Exception)
+		// object UnityEngine.ResourceManagement.ResourceManager.CreateOperation<object>(System.Type,int,UnityEngine.ResourceManagement.Util.IOperationCacheKey,System.Action<UnityEngine.ResourceManagement.AsyncOperations.IAsyncOperation>)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.ResourceManagement.ResourceManager.ProvideResource<!!0>(UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.ResourceManagement.ResourceManager.ProvideResource<object>(UnityEngine.ResourceManagement.ResourceLocations.IResourceLocation)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<!!0> UnityEngine.ResourceManagement.ResourceManager.StartOperation<!!0>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase<!!0>,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle)
+		// UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle<object> UnityEngine.ResourceManagement.ResourceManager.StartOperation<object>(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationBase<object>,UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle)
 	}
 }
