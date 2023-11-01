339b7eb6efd4f1b0ebf179bcd4bbf09d689c1c7c
Phong <manoking@126.com>
2023-11-01 15:51:12 +0800

    remove resources tool

--- Assets/Editor/Game/GameFrameworkConfigs.cs
+++ Assets/Editor/Game/GameFrameworkConfigs.cs
@@ -9,7 +9,6 @@ using GameFramework;
 using System.IO;
 using UnityEngine;
 using UnityGameFramework.Editor;
-using UnityGameFramework.Editor.ResourceTools;
 
 namespace Flower.Editor
 {
@@ -17,14 +16,5 @@ namespace Flower.Editor
     {
         [BuildSettingsConfigPath]
         public static string BuildSettingsConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/BuildSettings.xml"));
-
-        [ResourceCollectionConfigPath]
-        public static string ResourceCollectionConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceCollection.xml"));
-
-        [ResourceEditorConfigPath]
-        public static string ResourceEditorConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceEditor.xml"));
-
-        [ResourceBuilderConfigPath]
-        public static string ResourceBuilderConfig = Utility.Path.GetRegularPath(Path.Combine(Application.dataPath, "GameMain/Configs/ResourceBuilder.xml"));
     }
 }
