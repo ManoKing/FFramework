f454826ebee2fb0e63d280bb93b7c11ece16424c
Phong <manoking@126.com>
2024-02-07 14:28:07 +0800

    打包脚本接入，一键发布apk

---
+++ Assets/Editor/Build/AutoBuild.cs
@@ -0,0 +1,86 @@
+using HybridCLR.Editor;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEditor.AddressableAssets;
+using UnityEngine;
+
+public class AutoBuild : Editor
+{
+    public static string targetPath = "./build/";
+    public static string packName = "FFramework.apk";
+
+    [MenuItem("HybridCLR/Build/Build Android")]
+    public static void BuildAndroid()
+    {
+        Build();
+    }
+
+    [MenuItem("HybridCLR/Build/Build Android Project")]
+    public static void BuildAndroidProject()
+    {
+        packName = "FFrameworkProject";
+        EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
+        Build();
+    }
+
+    [MenuItem("HybridCLR/Build/Build Hot Resources")]
+    public static void BuildHotResources()
+    {
+        AutoHybridCLR.CheckHybridCLRInstall();
+        BuildAssetsCommand.BuildAndCopyABAOTHotUpdateDlls();
+        AutoAddressables.AutoUpdate();
+    }
+
+
+    public static void Build()
+    {
+        // 非播放模式
+        if (EditorApplication.isPlaying)
+        {
+            Debug.LogError("Editor is in Play mode.");
+            return;
+        }
+
+        // 检查是否安装
+        AutoHybridCLR.CheckHybridCLRInstall();
+
+        // 代码热更相关处理
+        BuildAssetsCommand.BuildAndCopyABAOTHotUpdateDlls();
+
+
+        // 资源相关处理
+        AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+        AddressableAssetSettings.BuildPlayerContent();
+
+
+        // 获取场景列表
+        List<string> levels = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+
+#if UNITY_EDITOR_OSX
+            string path =  Path.Combine(Application.dataPath.Replace("Assets", ""), scene.path).Replace("\\", "/");
+#else
+            string path = Path.Combine(Application.dataPath.Replace("Assets", ""), scene.path).Replace("/", "\\");
+
+#endif
+            if (File.Exists(path))
+            {
+                levels.Add(scene.path);
+            }
+            else
+            {
+                continue;
+            }
+
+        }
+
+        // 打包
+        BuildPipeline.BuildPlayer(levels.ToArray(), targetPath + packName, BuildTarget.Android,
+               EditorUserBuildSettings.development == true ? BuildOptions.Development : BuildOptions.None);
+
+    }
+}
---
+++ Assets/Editor/Build/AutoHybridCLR.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+using HybridCLR.Editor.Installer;
+public class AutoHybridCLR 
+{
+    private static InstallerController _controller;
+    private static InstallerController Controller
+    {
+        get
+        {
+            if (_controller == null)
+                _controller = new InstallerController();
+            return _controller;
+        }
+    }
+
+    /// <summary>
+    /// 检测CLRInstall是否已经安装并且编译，没安装就会执行安装步骤，并编译生成所有dll
+    /// </summary>
+    public static void CheckHybridCLRInstall()
+    {
+        if (!HasInstalledHybridCLR())
+        {
+            Debug.LogError("CheckHybridCLRInstall, HybridCLR未安装, 准备执行以下步骤");
+            HybridCLRInstall();
+            HybridCLRGeneralAll();
+        }   
+    }
+
+    public static bool HasInstalledHybridCLR()
+    {
+        return Controller.HasInstalledHybridCLR();
+    }
+
+
+    /// <summary>
+    /// 安装HybridCLR
+    /// </summary>
+    public static void HybridCLRInstall()
+    {
+        Debug.Log("1.HybridCLRInstall, 开始安装HybridCLR");
+        Controller.InstallDefaultHybridCLR();
+    }
+
+    /// <summary>
+    /// 编辑所有dll
+    /// </summary>
+    public static void HybridCLRGeneralAll()
+    {
+        Debug.Log("2.HybridCLRGeneralAll, HybridCLR生成Dll");
+        HybridCLR.Editor.Commands.PrebuildCommand.GenerateAll();
+    }
+}
