21ce490af574fa81dd85831f35f0b09e7b0fa510
fengyunlong <manoking@126.com>
2024-11-26 09:38:32 +0800

    修改打包脚本

--- Assets/Editor/Build/AutoBuild.cs
+++ Assets/Editor/Build/AutoBuild.cs
@@ -1,8 +1,10 @@
 using HybridCLR.Editor;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using YooAsset.Editor;
 
 public class AutoBuild : Editor
 {
@@ -28,7 +30,7 @@ public class AutoBuild : Editor
     {
         AutoHybridCLR.CheckHybridCLRInstall();
         BuildAssetsCommand.BuildAndCopyABAOTHotUpdateDlls();
-        //AutoAddressables.AutoUpdate();
+        BuildYooAsset(BuildTarget.Android, DateTime.Now.ToString("MM-dd-HH-mm"));
     }
 
 
@@ -47,11 +49,8 @@ public class AutoBuild : Editor
         // 代码热更相关处理
         BuildAssetsCommand.BuildAndCopyABAOTHotUpdateDlls();
 
-
         // 资源相关处理
-        //AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
-        //AddressableAssetSettings.BuildPlayerContent();
-
+        BuildYooAsset(BuildTarget.Android, DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"));
 
         // 获取场景列表
         List<string> levels = new List<string>();
@@ -80,5 +79,46 @@ public class AutoBuild : Editor
         BuildPipeline.BuildPlayer(levels.ToArray(), targetPath + packName, BuildTarget.Android,
                EditorUserBuildSettings.development == true ? BuildOptions.Development : BuildOptions.None);
 
+    }
+
+    private static void BuildYooAsset(BuildTarget buildTarget, string buildVersion, string packageName = "DefaultPackage", EBuildinFileCopyOption copyOp = EBuildinFileCopyOption.ClearAndCopyAll)
+    {
+        Debug.Log($"开始构建 : {buildTarget}");
+
+        var buildoutputRoot = AssetBundleBuilderHelper.GetDefaultBuildOutputRoot();
+        var streamingAssetsRoot = AssetBundleBuilderHelper.GetStreamingAssetsRoot();
+
+        // 构建参数
+        ScriptableBuildParameters scriptableBuildParameters = new ScriptableBuildParameters();
+        scriptableBuildParameters.BuildOutputRoot = buildoutputRoot;
+        scriptableBuildParameters.BuildinFileRoot = streamingAssetsRoot;
+        scriptableBuildParameters.BuildPipeline = EBuildPipeline.ScriptableBuildPipeline.ToString();
+        scriptableBuildParameters.BuildTarget = buildTarget;
+        scriptableBuildParameters.BuildMode = EBuildMode.IncrementalBuild;
+        scriptableBuildParameters.PackageName = packageName;
+        scriptableBuildParameters.PackageVersion = buildVersion;
+        scriptableBuildParameters.VerifyBuildingResult = true;
+        scriptableBuildParameters.EnableSharePackRule = true; //启用共享资源构建模式，兼容1.5x版本
+        scriptableBuildParameters.BuildinFileCopyOption = copyOp;
+        scriptableBuildParameters.BuildinFileCopyParams = string.Empty;
+        scriptableBuildParameters.CompressOption = ECompressOption.LZ4;
+
+        // Bundle格式
+        scriptableBuildParameters.FileNameStyle = EFileNameStyle.BundleName;
+        scriptableBuildParameters.EncryptionServices = null;
+
+
+
+        // 执行构建
+        ScriptableBuildPipeline pipeline = new ScriptableBuildPipeline();
+        var buildResult = pipeline.Run(scriptableBuildParameters, true);
+        if (buildResult.Success)
+        {
+            Debug.Log($"构建成功 : {buildResult.OutputPackageDirectory}");
+        }
+        else
+        {
+            Debug.LogError($"构建失败 : {buildResult.ErrorInfo}");
+        }
     }
 }
